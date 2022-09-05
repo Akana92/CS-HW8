ebbbf4471d57984601607fe983fe784fd81037e9
Akana <Akana92@gmail.com>
2022-09-05 20:25:10 +0600

    Add project files.

---
+++ BelieveOrNotBelieve/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BelieveOrNotBelieve
+{
+    //Класс для вопроса
+    [Serializable]
+    public class Question
+    {
+        string text;       // Текст вопроса
+        bool trueFalse;    // Правда или нет
+
+        public string Text { get { return text; } set { if (value.GetType() == typeof(string)) text = value; } }
+        public bool TrueFalse { get { return trueFalse; } set { if (value.GetType() == typeof(bool)) trueFalse = value; } }
+
+        
+        //Для сериализации приводится пустой конструктор
+        public Question()
+        {
+        }
+
+        //Метод записи вопроса
+        public Question(string text, bool trueFalse)
+        {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+    }
+
+}
